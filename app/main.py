32661c6ab1f5f76cfa4c1440d3bcc769f752c56d
JohnnySD <johnsdieckmann@gmail.com>
2022-09-06 07:17:48 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,12 +8,34 @@ from keep_alive import keep_alive
 
 load_dotenv()
 keep_alive()
+
+version = 'v-Unreleased'
 TOKEN = os.environ['DISCORD_TOKEN']
+description = ''
+intents = nextcord.Intents.default()
+intents.members = True
 
-class CustomClient(nextcord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+client = commands.Bot(command_prefix='/', description=description, intents=intents)
 
-client = CustomClient()
+def embed(title, description, reason):
+	nextcord.Embed(
+		title=title,
+		description=description,
+		color=0x360404
+	).set_author(
+		name=f"Crime.NET Bot {version}",
+		icon_url="https://avatars.githubusercontent.com/u/83386810?v=4"
+	).set_footer(
+		text=f"This message was sent because {reason}."
+	)
+
+@client.event
+async def on_ready():
+	print(f'Logged in as {client.user} (ID: {client.user.id})')
+	await client.change_presence(activity=nextcord.Game(name=f'{version}'))
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(embed(':ping_pong: Pong!', '{0}'.format(round(client.latency, 1)), 'the `ping` command was used'))
 
 client.run(TOKEN)
