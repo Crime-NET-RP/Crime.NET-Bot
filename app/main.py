192f2ee88c255d8b4102804f0d96e0cbd66d7cbc
JohnnySD <johnsdieckmann@gmail.com>
2022-09-05 19:04:18 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import os
 
 import nextcord
+from nextcord.ext import commands
 from boto.s3.connection import S3Connection
-from dotenv import load_dotenv
 from keep_alive import keep_alive
 
-load_dotenv()
 keep_alive()
 TOKEN = S3Connection(os.environ['DISCORD_TOKEN'])
 
