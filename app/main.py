e349d3cc158110a3a6ba7c9266eaa106335bc0ef
JohnnySD <johnsdieckmann@gmail.com>
2022-09-05 18:39:48 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import os
 
 import nextcord
+from boto.s3.connection import S3Connection
 from dotenv import load_dotenv
 from keep_alive import keep_alive
 
 load_dotenv()
 keep_alive()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = S3Connection(os.environ['DISCORD_TOKEN'])
 
 class CustomClient(nextcord.Client):
     async def on_ready(self):
