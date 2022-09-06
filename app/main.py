678d2593fccb26baba13f992389a5ddf922424b0
JohnnySD <johnsdieckmann@gmail.com>
2022-09-06 11:09:01 +1000

    Update main.py
    Kinda used the wrong image...

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ version = 'v-Unreleased'
 TOKEN = os.environ['DISCORD_TOKEN']
 description = ''
 intents = nextcord.Intents.default()
-intents.members = True
 
 client = commands.Bot(command_prefix='/', description=description, intents=intents)
 
@@ -24,7 +23,7 @@ def embed(title, description, reason):
 		color=0x360404
 	).set_author(
 		name=f"Crime.NET Bot {version}",
-		icon_url="https://avatars.githubusercontent.com/u/83386810?v=4"
+		icon_url="https://cdn.discordapp.com/icons/1001291176252543129/a_16d95fd874430473a19bd2506d91af10.gif?size=160"
 	).set_footer(
 		text=f"This message was sent because {reason}."
 	)
